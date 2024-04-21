cfba4cc5981c0a4eac3771cecf5f59767ba34af6
Logan Carpenter <59491011+Logan-0@users.noreply.github.com>
2024-04-21 15:45:30 -0400

    Updating more files

---
+++ Python/DiagonalDifference.py
@@ -0,0 +1,30 @@
+def diagonalDifference(arr):
+    # Init Vars
+    diagOne = 0
+    diagTwo = 0
+    length = len(arr)
+
+    # for each value in the row
+    for i in range(length):
+
+        # add the ith row &&&& ith column.
+        # creates downward diagonal to the right
+        #[x00][ 01][ 02]
+        #[ 10][x11][ 12]
+        #[ 20][ 21][x22]
+        diagOne += arr[i][i]
+
+        # add the ith row &&&& (length of the array -1 to start from the end) - ith column.
+        # creates upward diagonal to the left
+        #[ 00][ 01][x02]
+        #[ 10][x11][ 12]
+        #[x20][ 21][ 22]
+        diagTwo += arr[i][(length-1)-i]
+
+    #Check for correct values
+    print("Diag One: "+str(diagOne))
+    print("Diag Two: "+str(diagTwo))
+    
+    #Return difference and if it goes negative use math package to return absolute value
+    sum=abs(diagOne-diagTwo);
+    return sum
--- Python/Test.py
+++ Python/Test.py
@@ -2,6 +2,7 @@ from PlusMinus import plusMinus
 from MiniMaxSum import miniMaxSum
 from TimeConversion import timeConversion
 from LonelyInteger import lonelyInteger
+from DiagonalDifference import diagonalDifference
 
 # Generic Small Test Array, All values unique, containes pos and neg
 smallTestArray = [-987, 123, 7, 17, 26, -1, -759, 0]
@@ -11,6 +12,9 @@ smallDuplicateValueArray = [-999, -999, -999, 999, 999, 999, -42, -42, 11, 11, 7
 
 # Time String [00:00:00PM]
 timeString = "07:05:45PM"
+
+# Small Square Matrix
+smallSquareMatrix = [[11, 2, 4], [4, 5, 6], [10,8,-12]]
 
 if __name__ == "__main__":
     
@@ -30,6 +34,7 @@ if __name__ == "__main__":
     print("Testing LonelyInteger.py: ",lonelyIntegerResult)
     print()
 
-    
+    diagonalDifferenceResult = diagonalDifference(smallSquareMatrix)
+    print("Testing DiagonalDifference.py: ",diagonalDifferenceResult)
 
 
